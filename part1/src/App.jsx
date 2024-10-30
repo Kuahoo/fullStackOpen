50f99c499a7b7855f388ce6fd29d6fabc3d076a1
Kuahoo <kuahoo@pop-os.localdomain>
2024-10-30 13:56:17 -0700

    updated App.jsx with components

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,25 +1,57 @@
-const Hello = (props) => {
-    console.log(props)
+const Header = ({course}) => {
+    return (
+        <div>
+            <h1>{course}</h1>
+        </div>
+    )
+}
+
+const Part = ({part , exercises}) => {
     return (
         <div>
             <p>
-                Hello {props.name}, you are {props.age} years old
+                {part} {exercises}
             </p>
         </div>
     )
 }
 
-const App = () => {
-    const name = 'Peter'
-    const age = 10 
+const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) =>{
     return (
         <div>
-            <h1>Greetings</h1>
-            <Hello name='Maya' age = {26 + 10} />
-            <Hello name={name} age={age}/>
-            <Hello />
+            <Part part={part1} exercises={exercises1} />
+            <Part part={part2} exercises={exercises2} />
+            <Part part={part3} exercises={exercises3} />
         </div>
     )
+}
+
+const Total = ({exercises1, exercises2, exercises3}) => {
+    return (
+        <div>
+            <p>
+                Number of exercises {exercises1 + exercises2 + exercises3}
+            </p>
+        </div>
+    )
+}
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course = {course} />
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} 
+      exercises2={exercises2} exercises3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+    </div>
+  )
 }
 
 export default App
